05c750da3ae92859fe10af531272009ac49976b4
bossgeekgo <bossgeekgo>
2024-02-10 15:26:01 +0800

    add ding talk

---
+++ main/dingtalk.mjs
@@ -0,0 +1,29 @@
+import { dingTalkAccessToken } from "../runtime/dingtalk.mjs"
+
+export function createTextMessage (text) {
+  return JSON.stringify({
+    "msgtype": "text",
+    "text": {
+      "content": `${text}【geekgo】`
+    }
+  })
+}
+
+export async function requestDingTalkNotify (body) {
+  const url = new URL(`https://oapi.dingtalk.com/robot/send`)
+  url.searchParams.append(
+    'access_token',
+    dingTalkAccessToken
+  )
+
+  return await fetch(
+    url,
+    {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    }
+  )
+}
