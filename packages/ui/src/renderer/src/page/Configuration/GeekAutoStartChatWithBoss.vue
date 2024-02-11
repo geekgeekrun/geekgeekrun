f2ffcbc77d01016557d6e22a5aba8b1f041ae50e
bossgeekgo <bossgeekgo>
2024-02-11 22:09:16 +0800

    add vue-router

--- packages/ui/src/renderer/src/page/Configuration/GeekAutoStartChatWithBoss.vue
+++ packages/ui/src/renderer/src/page/Configuration/GeekAutoStartChatWithBoss.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-  <RouterView />
+  <div>Hello !!!</div>
 </template>
---
+++ packages/ui/src/renderer/src/router/index.ts
@@ -0,0 +1,40 @@
+import { defineComponent, h } from 'vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: defineComponent({ setup: () => h('div') }),
+    redirect: '/configuration',
+    children: []
+  },
+  {
+    path: '/configuration',
+    component: import('@renderer/page/Configuration/index.vue'),
+    redirect: '/configuration/GeekAutoStartChatWithBoss',
+    children: [
+      {
+        path: 'GeekAutoStartChatWithBoss',
+        component: import('@renderer/page/Configuration/GeekAutoStartChatWithBoss.vue'),
+        meta: {
+          title: '"geek auto start chat with boss" configuration'
+        }
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+router.afterEach((to) => {
+  if (to.meta?.title) {
+    document.title = `${to.meta.title} - BossGeekGo`
+  } else {
+    document.title = `BossGeekGo`
+  }
+})
+
+export default router
