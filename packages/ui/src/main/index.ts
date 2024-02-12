9f2090ac458ec354f88260ea2d3eb5c0a1816964
bossgeekgo <bossgeekgo>
2024-02-12 13:01:32 +0800

    adjust the env name; add the dedicate script to run auto start chat

--- packages/ui/src/main/index.ts
+++ packages/ui/src/main/index.ts
@@ -1,7 +1,7 @@
 import { runAutoChat } from './flow/GEEK_AUTO_START_CHAT_WITH_BOSS'
 import { openSettingWindow } from './flow/OPEN_SETTING_WINDOW'
 
-const runMode = process.env.BOSSGEEKGO_RUN_MODE
+const runMode = process.env.MAIN_BOSSGEEKGO_RUN_MODE
 switch (runMode) {
   case 'geekAutoStartWithBoss': {
     runAutoChat()
