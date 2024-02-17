8eef881211eb39ab73ad4ff3e1fd8b1b27d1fecc
bossgeekgo <bossgeekgo>
2024-02-17 22:17:23 +0800

    add bossgeekgo-external-node-runtime-dependencies package

---
+++ packages/ui/external-node-runtime-dependencies/index.cjs
@@ -0,0 +1,5 @@
+Object.assign(module.exports, {
+  puppeteerExtra: require('puppeteer-extra'),
+  PuppeteerExtraPluginStealth: require('puppeteer-extra-plugin-stealth'),
+  puppeteerManager: require('@puppeteer/browsers')
+})
---
+++ packages/ui/external-node-runtime-dependencies/index.mjs
@@ -0,0 +1,4 @@
+export * as puppeteerExtra from 'puppeteer-extra'
+export * as PuppeteerExtraPluginStealth from 'puppeteer-extra-plugin-stealth'
+export * as puppeteerManager from '@puppeteer/browsers'
+
