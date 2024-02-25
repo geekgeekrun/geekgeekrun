17cdbd871c7b0ebe7a576b53ac49a883050e243b
geekgeekrun <geekgeekrun>
2024-02-25 14:39:48 +0800

    make find-chrome-bin as external dependencies to resolve production build runtime error

--- packages/ui/src/main/flow/CHECK_AND_DOWNLOAD_DEPENDENCIES/check-and-locate-existed-chromium-executable.ts
+++ packages/ui/src/main/flow/CHECK_AND_DOWNLOAD_DEPENDENCIES/check-and-locate-existed-chromium-executable.ts
@@ -1,7 +1,23 @@
-import { findChrome } from "find-chrome-bin";
+import { is } from '@electron-toolkit/utils'
+import electron from 'electron'
 import * as os from 'node:os'
+import path from 'node:path'
 
 export default async function findAndLocateExistedChromiumExecutable() {
+  let findChrome: typeof import('find-chrome-bin').findChrome
+  if (is.dev) {
+    findChrome = (await import('find-chrome-bin')).findChrome
+  } else {
+    findChrome = (
+      await import(
+        path.resolve(
+          electron.app.getAppPath(),
+          '..',
+          'external-node-runtime-dependencies/index.mjs'
+        )
+      )
+    ).findChromeBin.findChrome
+  }
   // For windows, try to find Edge(chromium)
   if (os.platform() === 'win32') {
     // TODO: handle windows
