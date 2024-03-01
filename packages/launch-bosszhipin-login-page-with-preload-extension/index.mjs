7f2e241773fc09f5e807347c3ea87b8609685e17
geekgeekrun <geekgeekrun>
2024-03-01 23:45:14 +0800

    change package name from launch-browser-with-preload-extension to launch-bosszhipin-login-page-with-preload-extension

--- packages/launch-bosszhipin-login-page-with-preload-extension/index.mjs
+++ packages/launch-bosszhipin-login-page-with-preload-extension/index.mjs
@@ -7,6 +7,7 @@ import {
   sleepWithRandomDelay
 } from '@geekgeekrun/utils/sleep.mjs'
 import extractZip from 'extract-zip'
+import { blockNavigation } from '@geekgeekrun/utils/puppeteer/block-navigation.mjs'
 
 import fs from 'node:fs'
 import os from 'node:os'
@@ -50,10 +51,36 @@ async function main() {
   const browser = await puppeteer.launch({
     headless: false,
     args: [
-      `--load-extension=${editThisCookieExtensionPath}`,
-      `https://www.zhipin.com/web/user/`
+      `--load-extension=${editThisCookieExtensionPath}`
     ]
   })
+
+  const closeAttachedSet = new WeakSet()
+  browser.on('targetcreated', async function closeNewTabs(target) {
+    let targetBrowser = target.browser();
+    const pages = await targetBrowser.pages()
+    console.log(pages)
+    for (let i = 1; i < pages.length; i++) {
+      const page = pages[i]
+      if (!closeAttachedSet.has(page)) {
+        closeAttachedSet.add(page)
+        page.once('domcontentloaded', () => {
+          page.evaluate(() => {
+            window.close()
+          })
+        })
+      }
+    }
+  })
+
+  const [page] = await browser.pages();
+
+  page.once('close', () => {
+    browser.close()
+  })
+
+  const { dispose: disposeNavigation } = await blockNavigation(page, (req) => !req.url().startsWith('https://www.zhipin.com'))
+  await page.goto('https://www.zhipin.com/web/user/');
 }
 
 main()
