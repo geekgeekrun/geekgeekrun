ded1c9e8b2abc3e2bd48e56256c67b0522c9a5a0
geekgeekrun <geekgeekrun>
2024-03-16 03:27:09 +0800

    add the PUPPETEER_IS_NOT_EXECUTABLE enum for cli

--- packages/run-core-of-geek-auto-start-chat-with-boss/enums.mjs
+++ packages/run-core-of-geek-auto-start-chat-with-boss/enums.mjs
@@ -4,7 +4,8 @@ export const AUTO_CHAT_ERROR_EXIT_CODE = (() => {
     COOKIE_INVALID: 81,
     LOGIN_STATUS_INVALID: 82,
     ERR_INTERNET_DISCONNECTED: 83,
-    ACCESS_IS_DENIED: 84
+    ACCESS_IS_DENIED: 84,
+    PUPPETEER_IS_NOT_EXECUTABLE: 85
   }
   const kvList = Object.entries(enums)
   
