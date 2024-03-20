64d2ef4efe4c3b269ac320435895aa2cddf23c97
geekgeekrun <geekgeekrun>
2024-03-21 01:49:37 +0800

    fix the issue that findChromeBin not support commonjs so when import a error is thrown so that app cannot launch

--- packages/ui/external-node-runtime-dependencies/index.cjs
+++ packages/ui/external-node-runtime-dependencies/index.cjs
@@ -2,6 +2,5 @@ Object.assign(module.exports, {
   puppeteerExtra: require('puppeteer-extra'),
   PuppeteerExtraPluginStealth: require('puppeteer-extra-plugin-stealth'),
   puppeteerManager: require('@puppeteer/browsers'),
-  findChromeBin: require('find-chrome-bin'),
   typeorm: require('typeorm')
 })
