af92ef67187a32598b668d9cd7cae4837225629f
geekgeekrun <geekgeekrun>
2024-03-20 09:42:32 +0800

    add typeorm in external-node-runtime-dependencies

--- packages/ui/external-node-runtime-dependencies/index.cjs
+++ packages/ui/external-node-runtime-dependencies/index.cjs
@@ -2,5 +2,6 @@ Object.assign(module.exports, {
   puppeteerExtra: require('puppeteer-extra'),
   PuppeteerExtraPluginStealth: require('puppeteer-extra-plugin-stealth'),
   puppeteerManager: require('@puppeteer/browsers'),
-  findChromeBin: require('find-chrome-bin')
+  findChromeBin: require('find-chrome-bin'),
+  typeorm: require('typeorm')
 })
