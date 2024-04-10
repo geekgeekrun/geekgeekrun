307aa93f426a1b439d9294c8c118e3957f31bc57
geekgeekrun-maintainer <166113191+geekgeekrun-maintainer@users.noreply.github.com>
2024-04-10 00:12:04 +0000

    skip buildUiOnCurrentPlatform when run `pnpm run release`

--- packages/ui/scripts/release-new-version.mjs
+++ packages/ui/scripts/release-new-version.mjs
@@ -1,8 +1,8 @@
-import buildUiOnCurrentPlatform from './steps/build-ui-on-current-platform.mjs';
+// import buildUiOnCurrentPlatform from './steps/build-ui-on-current-platform.mjs';
 import increasePackageVersion from './steps/increase-package-version.mjs'
 import releaseVersion from './steps/release-version.mjs'
 ;(async () => {
   await increasePackageVersion()
-  await buildUiOnCurrentPlatform()
+  // await buildUiOnCurrentPlatform()
   await releaseVersion()
 })()
