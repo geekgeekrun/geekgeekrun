f366d5777206272bcbe6783488d29b1df9b3a9b0
geekgeekrun <geekgeekrun>
2024-10-19 09:16:50 +0800

    add statement to remove all views

--- packages/sqlite-plugin/src/migrations/1729182577167-UpdateChatStartupLogTable.ts
+++ packages/sqlite-plugin/src/migrations/1729182577167-UpdateChatStartupLogTable.ts
@@ -1,6 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-const dropViewSql = `DROP VIEW IF EXISTS "v_chat_startup_log"`;
+const dropViewSql = `
+DROP VIEW IF EXISTS "v_boss_library";
+DROP VIEW IF EXISTS "v_chat_startup_log";
+DROP VIEW IF EXISTS "v_company_library";
+DROP VIEW IF EXISTS "v_job_library";
+`;
 
 export class UpdateChatStartupLogTable1729182577167 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
