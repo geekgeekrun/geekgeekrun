7a3bddbd0c417499c8f48942fadeb4d55b5eefea
geekgeekrun <geekgeekrun>
2024-10-24 02:01:25 +0800

    replace magic string in gtag

--- packages/ui/electron.vite.config.ts
+++ packages/ui/electron.vite.config.ts
@@ -1,10 +1,12 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, loadEnv } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
 import transformerDirective from "@unocss/transformer-directives";
+import Replace from 'unplugin-replace/vite'
 
+process.env = { ...process.env, ...loadEnv(process.env.NODE_ENV!, process.cwd()) }
 export default defineConfig({
   main: {
     build: {
@@ -15,6 +17,21 @@ export default defineConfig({
     plugins: [
       externalizeDepsPlugin({
         exclude: ['@geekgeekrun/geek-auto-start-chat-with-boss', '@geekgeekrun/dingtalk-plugin', '@geekgeekrun/utils', 'find-chrome-bin', '@geekgeekrun/launch-bosszhipin-login-page-with-preload-extension']
+      }),
+      Replace({
+        delimiters: ['', ''],
+        sourcemap: true,
+        include: ['**/src/main/utils/gtag/Analytics.ts'],
+        values: [
+          {
+            find: /<measurement_id>/g,
+            replacement: process.env.VITE_APP_GTAG_MEASUREMENT_ID as string,
+          },
+          {
+            find: /<api_secret>/g,
+            replacement: process.env.VITE_APP_GTAG_API_SECRET as string,
+          }
+        ]
       })
     ]
   },
