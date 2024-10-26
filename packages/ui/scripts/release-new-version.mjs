fa45d4ec3568ba53eea609cab9481a46e148ab31
geekgeekrun <geekgeekrun>
2024-10-26 14:50:29 +0800

    update build script to enter release type

--- packages/ui/scripts/release-new-version.mjs
+++ packages/ui/scripts/release-new-version.mjs
@@ -1,6 +1,10 @@
 import increasePackageVersion from './steps/increase-package-version.mjs'
 import releaseVersion from './steps/release-version.mjs'
+import prompt from 'prompt-sync'
 ;(async () => {
-  await increasePackageVersion()
+  const releaseType = prompt()(
+    'Enter the release type (default: prerelease, available: prerelease / prepatch / patch / preminor / minor / premajor / major): '
+  )
+  await increasePackageVersion(releaseType || 'prerelease')
   await releaseVersion()
 })()
